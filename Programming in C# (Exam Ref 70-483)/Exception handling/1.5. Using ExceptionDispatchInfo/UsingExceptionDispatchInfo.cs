839cc2b4032fe297a7836838b44f2cd0dbc582aa
Martin <flextry@gmail.com>
2016-12-03 21:20:34 +0200

    Added codes for 2.6. Manage objects life cycle

---
+++ Programming in C# (Exam Ref 70-483)/Exception handling/1.5. Using ExceptionDispatchInfo/UsingExceptionDispatchInfo.cs
@@ -0,0 +1,25 @@
+﻿namespace ProgrammingInCSharp.UsingExceptionDispatchInfo
+{
+    using System;
+    using System.Runtime.ExceptionServices;
+
+    public class UsingExceptionDispatchInfo
+    {
+        private static void Main()
+        {
+            ExceptionDispatchInfo possibleException = null;
+
+            try
+            {
+                var text = Console.ReadLine();
+                int.Parse(text);
+            }
+            catch (FormatException ex)
+            {
+                possibleException = ExceptionDispatchInfo.Capture(ex);
+            }
+
+            possibleException?.Throw();
+        }
+    }
+}
---
+++ Programming in C# (Exam Ref 70-483)/Object life cycle/2.6. Disposable and finalizer/DisposableAndFinalizer.cs
@@ -0,0 +1,46 @@
+﻿namespace ProgrammingInCSharp.DisposableAndFinalizer
+{
+    using System;
+    using System.IO;
+
+    public class DisposableAndFinalizer
+    {
+        private static void Main()
+        {
+        }
+    }
+
+    public class UnmanagedWrapper : IDisposable
+    {
+        public FileStream Stream { get; private set; }
+
+        public UnmanagedWrapper()
+        {
+            //this.Stream = ...
+        }
+
+        ~UnmanagedWrapper()
+        {
+            this.Dispose(false);
+        }
+
+        public void Close()
+        {
+            this.Dispose();
+        }
+
+        public void Dispose()
+        {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                this.Stream?.Close();
+            }
+        }
+    }
+}
