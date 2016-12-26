0a331de3704946d994436066399441a3497038e1
Martin <flextry@gmail.com>
2016-12-26 21:29:06 +0200

    Book: Pro ASP.NET MVC 4 - Added codes for Chapter 16: Filters

--- Pro ASP.NET MVC 4/Routes/14.1. CustomRouteHandler.Example/Controllers/HomeController.cs
+++ Pro ASP.NET MVC 4/Routes/14.1. CustomRouteHandler.Example/Controllers/HomeController.cs
@@ -1,10 +1,13 @@
 ﻿namespace CustomRouteHandler.Example.Controllers
 {
     using System.Web.Mvc;
+    using CustomRouteHandler.Example.Infrastructure;
 
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        [CustomFilter]
+        [OutputCache(Duration = 5, VaryByParam = "*")]
+        public ActionResult Index(string id)
         {
             return this.View();
         }
---
+++ Pro ASP.NET MVC 4/Routes/14.1. CustomRouteHandler.Example/Infrastructure/CustomFilterAttribute.cs
@@ -0,0 +1,24 @@
+﻿namespace CustomRouteHandler.Example.Infrastructure
+{
+    using System;
+    using System.Diagnostics;
+    using System.Web.Mvc;
+
+    [AttributeUsage(AttributeTargets.Method)]
+    public class CustomFilterAttribute : ActionFilterAttribute
+    {
+        private Stopwatch stopWatch;
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            this.stopWatch = Stopwatch.StartNew();
+        }
+
+        public override void OnResultExecuting(ResultExecutingContext filterContext)
+        {
+            this.stopWatch.Stop();
+
+            filterContext.Controller.TempData["ElapsedTime"] = this.stopWatch.Elapsed.Milliseconds;
+        }
+    }
+}
