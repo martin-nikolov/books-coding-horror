fc2890a080f9f9b71a6288e0c9683198f6523f29
Martin <flextry@gmail.com>
2016-12-28 23:45:23 +0200

    Added custom error handling in demo for routing

---
+++ Pro ASP.NET MVC 4/Routes/14.1. CustomRouteHandler.Example/Controllers/ErrorController.cs
@@ -0,0 +1,36 @@
+﻿namespace CustomRouteHandler.Example.Controllers
+{
+    using System.Web.Mvc;
+
+    public class ErrorController : Controller
+    {
+        public ActionResult Index(string aspxerrorpath)
+        {
+            if (string.IsNullOrWhiteSpace(aspxerrorpath))
+            {
+                return this.RedirectToAction("Index", "Home");
+            }
+
+            this.ViewBag.Title = "Error occurs!";
+
+            return this.View("Index");
+        }
+
+        public ActionResult NotFound(string aspxerrorpath)
+        {
+            if (string.IsNullOrWhiteSpace(aspxerrorpath))
+            {
+                return this.RedirectToAction("Index", "Home");
+            }
+
+            this.ViewBag.Title = "Error - not found!";
+
+            return this.View("NotFound");
+        }
+
+        public ActionResult Error()
+        {
+            return this.View();
+        }
+    }
+}
