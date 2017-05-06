17cd08b408dbe5a21ed266b715ce4832d680c0c5
flextry <flextry@gmail.com>
2017-05-06 13:19:49 +0300

    Book: Concurrency in C# - Cookbook
    1) Added demo for exception handling - single and multiple exceptions

---
+++ Concurrency in C# - Cookbook/Async Basics/2. 4. Exception handling/MainEntryPoint.cs
@@ -0,0 +1,63 @@
+﻿namespace ExceptionHandling
+{
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public class MainEntryPoint
+    {
+        internal static void Main()
+        {
+            ObserveOneExceptionAsync().Wait();
+            ObserveAllExceptionsAsync().Wait();
+        }
+
+        private static async Task ObserveOneExceptionAsync()
+        {
+            var task1 = ThrowNotImplementedException();
+            var task2 = ThrowInvalidOperationException();
+
+            Console.WriteLine("Waiting the tasks...");
+
+            try
+            {
+                await Task.WhenAll(task1, task2);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: {0}\n", ex.GetType().Name);
+            }
+        }
+
+        private static async Task ObserveAllExceptionsAsync()
+        {
+            var task1 = ThrowNotImplementedException();
+            var task2 = ThrowInvalidOperationException();
+
+            Console.WriteLine("Waiting the tasks...");
+
+            var allTasks = Task.WhenAll(task1, task2);
+
+            try
+            {
+                await allTasks;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exceptions: {0}\n", string.Join(", ", allTasks.Exception.InnerExceptions.Select(x => x.GetType().Name)));
+            }
+        }
+
+        /// !IMPORTANT: if we skip the 'async' keyword the exception is thrown immediately after method invocation (line 17 and line 34)
+        private static async Task<Task> ThrowNotImplementedException()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// !IMPORTANT: if we skip the 'async' keyword the exception is thrown immediately after method invocation (line 18 and line 35)
+        private static async Task<Task> ThrowInvalidOperationException()
+        {
+            throw new InvalidOperationException();
+        }
+    }
+}
