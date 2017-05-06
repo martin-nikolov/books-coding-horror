107d8a2c95b5a550cb29c2039bfb7f64ea0727af
flextry <flextry@gmail.com>
2017-05-06 18:18:38 +0300

    Book: Concurrency in C# - Cookbook
    1) Added demo with Parallel.Invoke
    2) Added demo with AttachedToParent and .ContinueWith

---
+++ Concurrency in C# - Cookbook/Parallel Basics/3. 1. Parallel Invocation/MainEntryPoint.cs
@@ -0,0 +1,55 @@
+﻿namespace ParallelInvocation
+{
+    using System;
+    using System.Diagnostics;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public class MainEntryPoint
+    {
+        internal static void Main()
+        {
+            var array = new int[1000];
+
+            // CPU intensive processing
+            ProcessArray(array);
+            ProcessArrayInParallel(array);
+        }
+
+        private static void ProcessArray(int[] array)
+        {
+            var stopWatch = Stopwatch.StartNew();
+            {
+                ProcessPartialArray(array, 0, array.Length/2);
+                ProcessPartialArray(array, array.Length/2, array.Length);
+            }
+
+            Console.WriteLine("Elapsed time: {0}\n", stopWatch.Elapsed);
+        }
+
+        private static void ProcessArrayInParallel(int[] array)
+        {
+            var stopWatch = Stopwatch.StartNew();
+            {
+                Parallel.Invoke(
+                    () => ProcessPartialArray(array, 0, array.Length / 2),
+                    () => ProcessPartialArray(array, array.Length / 2, array.Length));
+            }
+
+            Console.WriteLine("Elapsed time: {0}", stopWatch.Elapsed);
+        }
+
+        private static void ProcessPartialArray(int[] array, int begin, int end)
+        {
+            var threadId = Thread.CurrentThread.ManagedThreadId;
+            Console.WriteLine("Managed thread id: {0}", threadId);
+
+            for (int i = begin, sum = 0; i < end; i++)
+            {
+                Thread.Sleep(10);
+
+                sum += array[i];
+            }
+        }
+    }
+}
---
+++ Concurrency in C# - Cookbook/Parallel Basics/3. 2. Dynamic Parallelism/MainEntryPoint.cs
@@ -0,0 +1,85 @@
+﻿namespace DynamicParallelism
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Nito.AsyncEx;
+
+    public class MainEntryPoint
+    {
+        private static void Main()
+        {
+            try
+            {
+                AsyncContext.Run(async () => await MainAsync());
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex.Message);
+            }
+        }
+
+        private static async Task MainAsync()
+        {
+            await UsingAttachedToParentTask();
+            await UsingContinuationTask();
+        }
+
+        private static async Task UsingAttachedToParentTask()
+        {
+            var parentTask = Task.Factory.StartNew(ParentWithContinuationTaskAction,
+                                                   CancellationToken.None,
+                                                   TaskCreationOptions.None,
+                                                   TaskScheduler.Default);
+
+            await parentTask;
+        }
+
+        private static async Task UsingContinuationTask()
+        {
+            var parentTask = Task.Factory.StartNew(ParentTaskAction,
+                                                   CancellationToken.None,
+                                                   TaskCreationOptions.None,
+                                                   TaskScheduler.Default);
+
+            var continuationTask = parentTask.ContinueWith(t => ContinuationTaskAction(),
+                                                           CancellationToken.None,
+                                                           TaskContinuationOptions.AttachedToParent,
+                                                           TaskScheduler.Default);
+
+            await continuationTask;
+        }
+
+        private static void ParentTaskAction()
+        {
+            Console.WriteLine("\nParent task started...");
+
+            Thread.Sleep(1000);
+
+            Console.WriteLine("Parent task was completed successfully. Managed thread id: {0}", Thread.CurrentThread.ManagedThreadId);
+        }
+
+        private static void ParentWithContinuationTaskAction()
+        {
+            Console.WriteLine("Parent task started...");
+
+            Thread.Sleep(1000);
+
+            Task.Factory.StartNew(ContinuationTaskAction, CancellationToken.None, TaskCreationOptions.AttachedToParent, TaskScheduler.Default);
+            Task.Factory.StartNew(ContinuationTaskAction, CancellationToken.None, TaskCreationOptions.AttachedToParent, TaskScheduler.Default);
+            Task.Factory.StartNew(ContinuationTaskAction, CancellationToken.None, TaskCreationOptions.AttachedToParent, TaskScheduler.Default);
+            Task.Factory.StartNew(ContinuationTaskAction, CancellationToken.None, TaskCreationOptions.AttachedToParent, TaskScheduler.Default);
+
+            Console.WriteLine("Parent task was completed successfully. Managed thread id: {0}", Thread.CurrentThread.ManagedThreadId);
+        }
+
+        private static void ContinuationTaskAction()
+        {
+            Console.WriteLine("Child task started...");
+
+            Thread.Sleep(3000);
+
+            Console.WriteLine("Child task was completed succesfully. Managed thread id: {0}", Thread.CurrentThread.ManagedThreadId);
+        }
+    }
+}
