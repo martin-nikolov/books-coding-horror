345a2d3283ee2ba523e1b842a3f9df96caa78c58
flextry <flextry@gmail.com>
2017-05-06 12:45:48 +0300

    Book: Concurrency in C# - Cookbook 1) Added demo for simple timeout 2) Added demo for linear and concurrent processing of task in collection

---
+++ Concurrency in C# - Cookbook/Async Basics/2. 2. Simple timeout/MainEntryPoint.cs
@@ -0,0 +1,63 @@
+﻿namespace SimpleTimeout
+{
+    using System;
+    using System.Threading.Tasks;
+    using Nito.AsyncEx;
+
+    public class MainEntryPoint
+    {
+        internal static void Main()
+        {
+            try
+            {
+                AsyncContext.Run(async () => await MainAsync());
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex.Message);
+            }
+        }
+
+        private static async Task MainAsync()
+        {
+            /*
+             * 1. Completed task within specified timeout. 
+             */
+
+            var task1 = GetSampleTask(runtimeInSeconds: 2);
+            var completedTask = await RunWithTimeout(task1, 3);
+
+            Console.WriteLine("TASK 1 ---> Result: {0}\n", await completedTask);
+
+            /*
+             * 2. Task exceeded timeout interval. 
+             */
+
+            var task2 = GetSampleTask(runtimeInSeconds: 3);
+            var exceededTimeoutTask = await RunWithTimeout(task2, 2);
+
+            // Exception is thrown
+            Console.WriteLine("TASK 2 ---> Result: {0}", await exceededTimeoutTask);
+        }
+
+        private static async Task<int> GetSampleTask(int runtimeInSeconds)
+        {
+            await Task.Delay(TimeSpan.FromSeconds(runtimeInSeconds));
+
+            return 1;
+        }
+
+        private static async Task<Task<T>> RunWithTimeout<T>(Task<T> task, int timeoutInSeconds)
+        {
+            var timeoutTask = Task.Delay(TimeSpan.FromSeconds(timeoutInSeconds));
+
+            var completedTask = await Task.WhenAny(task, timeoutTask);
+            if (completedTask == timeoutTask)
+            {
+                throw new TimeoutException("Timeout interval was exceeded.");
+            }
+
+            return completedTask as Task<T>;
+        }
+    }
+}
---
+++ Concurrency in C# - Cookbook/Async Basics/2.1. Concurrent processing/MainEntryPoint.cs
@@ -0,0 +1,93 @@
+﻿namespace ConcurrentProcessing
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Nito.AsyncEx;
+
+    public class MainEntryPoint
+    {
+        internal static void Main()
+        {
+            try
+            {
+                AsyncContext.Run(async () => await MainAsync());
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex);
+            }
+        }
+
+        private static async Task MainAsync()
+        {
+            /*
+             * 1. Linear processing
+             */
+
+            await LinearProcessing();
+
+            /*
+             * 2. Concurrent processing
+             */
+
+            await ConcurrentProcessing();
+        }
+
+        private static async Task LinearProcessing()
+        {
+            var stopWatch = Stopwatch.StartNew();
+            {
+                var task1 = DelayAndReturnAsync(5);
+                var task2 = DelayAndReturnAsync(2);
+                var task3 = DelayAndReturnAsync(5);
+
+                var tasks = new List<Task<int>>() { task1, task2, task3 };
+
+                // Await each task in order
+                foreach (var task in tasks)
+                {
+                    var result = await task;
+
+                    Console.WriteLine(result);
+                }
+            }
+
+            Console.WriteLine("LINEAR PROCESSING ---> Elapsed time: {0}\n", stopWatch.Elapsed);
+        }
+
+        private static async Task ConcurrentProcessing()
+        {
+            var stopWatch = Stopwatch.StartNew();
+            {
+                var task1 = DelayAndReturnAsync(5);
+                var task2 = DelayAndReturnAsync(2);
+                var task3 = DelayAndReturnAsync(5);
+
+                var tasks = new List<Task<int>>() { task1, task2, task3 };
+
+                var processingTasks = tasks.Select(async t =>
+                {
+                    var result = await t;
+
+                    Console.WriteLine(result);
+                })
+                .ToArray();
+
+                // Await all processing to complete
+                await Task.WhenAll(processingTasks);
+            }
+
+            Console.WriteLine("CONCURRENT PROCESSING ---> Elapsed time: {0}\n", stopWatch.Elapsed);
+        }
+
+        private static async Task<int> DelayAndReturnAsync(int seconds)
+        {
+            await Task.Delay(TimeSpan.FromSeconds(seconds));
+
+            return seconds;
+        }
+    }
+}
