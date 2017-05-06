23392ad0d46201597e25e17d01a9d39b8b9be9af
flextry <flextry@gmail.com>
2017-05-06 12:57:07 +0300

    Book: Concurrency in C# - Cookbook
    1) Added demo for reporting progress via IProgress

---
+++ Concurrency in C# - Cookbook/Async Basics/2. 3. Report Progress/MainEntryPoint.cs
@@ -0,0 +1,37 @@
+﻿namespace ReportProgress
+{
+    using System;
+    using System.Threading.Tasks;
+
+    public class MainEntryPoint
+    {
+        internal static void Main()
+        {
+            CallMyMethodAsync().Wait();
+        }
+
+        private static async Task CallMyMethodAsync()
+        {
+            var progress = new Progress<int>();
+
+            progress.ProgressChanged += (sender, args) =>
+            {
+                Console.WriteLine("{0}%", args);
+            };
+
+            await MyMethodAsync(progress);
+        }
+
+        private static async Task MyMethodAsync(IProgress<int> progress = null)
+        {
+            int percentComplete = 0;
+
+            while (percentComplete < 100)
+            {
+                progress?.Report(++percentComplete);
+
+                await Task.Delay(10);
+            }
+        }
+    }
+}
