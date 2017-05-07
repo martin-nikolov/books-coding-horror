c00bfb7eea08a9ee9ab2e8bad3cbc0ca3bc82c87
flextry <flextry@gmail.com>
2017-05-07 12:22:25 +0300

    Book: Concurrency in C# - Cookbook
    1) Added demo for Immutable stack
    2) Added demo for Immutable queue

---
+++ Concurrency in C# - Cookbook/Collections/8. 1. Immutable Queue/MainEntryPoint.cs
@@ -0,0 +1,33 @@
+﻿namespace ImmutableQueue
+{
+    using System;
+    using System.Collections.Immutable;
+    using System.Linq;
+
+    /*
+     * This package provides collections that are thread safe and guaranteed to never change their contents, 
+     * also known as immutable collections. Like strings, any methods that perform modifications will not change 
+     * the existing instance but instead return a new instance. For efficiency reasons, the implementation uses 
+     * a sharing mechanism to ensure that newly created instances share as much data as possible with the previous 
+     * instance while ensuring that operations have a predictable time complexity.
+     */
+
+    public class MainEntryPoint
+    {
+        internal static void Main()
+        {
+            var queue = ImmutableQueue<int>.Empty;
+
+            queue = queue.Enqueue(1);
+            queue = queue.Enqueue(7);
+
+            Console.WriteLine("Queue: {0}", string.Join(", ", queue));
+
+            int nextItem;
+            queue = queue.Dequeue(out nextItem);
+
+            Console.WriteLine("Next item: {0}", nextItem);
+            Console.WriteLine("Count: {0}", queue.Count());
+        }
+    }
+}
---
+++ Concurrency in C# - Cookbook/Collections/8. 1. Immutable Stack/MainEntryPoint.cs
@@ -0,0 +1,51 @@
+﻿namespace ImmutableStack
+{
+    using System;
+    using System.Collections.Immutable;
+    using System.Linq;
+
+    /*
+     * This package provides collections that are thread safe and guaranteed to never change their contents, 
+     * also known as immutable collections. Like strings, any methods that perform modifications will not change 
+     * the existing instance but instead return a new instance. For efficiency reasons, the implementation uses 
+     * a sharing mechanism to ensure that newly created instances share as much data as possible with the previous 
+     * instance while ensuring that operations have a predictable time complexity.
+     */
+
+    public class MainEntryPoint
+    {
+        internal static void Main()
+        {
+            FirstDemo();
+            SecondDemo();
+        }
+
+        private static void FirstDemo()
+        {
+            var stack = ImmutableStack<int>.Empty;
+
+            stack = stack.Push(1);
+            stack = stack.Push(7);
+
+            Console.WriteLine("Stack: {0}", string.Join(", ", stack));
+
+            int lastItem;
+            stack = stack.Pop(out lastItem);
+
+            Console.WriteLine("Last item: {0}", lastItem);
+            Console.WriteLine("Count: {0}", stack.Count());
+            Console.WriteLine("{0}", new string('-', 30));
+        }
+
+        private static void SecondDemo()
+        {
+            var stack = ImmutableStack<int>.Empty;
+            stack = stack.Push(1);
+
+            var biggerStack = stack.Push(7);
+
+            Console.WriteLine("Stack: {0}", string.Join(", ", stack));
+            Console.WriteLine("Bigger stack: {0}\n", string.Join(", ", biggerStack));
+        }
+    }
+}
