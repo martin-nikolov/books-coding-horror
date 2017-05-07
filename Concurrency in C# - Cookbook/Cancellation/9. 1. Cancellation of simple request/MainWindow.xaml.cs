3be576edb0668ac850d992116bcc3ba3336df571
flextry <flextry@gmail.com>
2017-05-07 22:21:35 +0300

    Book: Concurrency in C# - Cookbook 1) Added demo with Cancellation token source

---
+++ Concurrency in C# - Cookbook/Cancellation/9. 1. Cancellation of simple request/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿namespace CancellationOfSimpleRequest
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using System.Windows;
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private CancellationTokenSource cancellationTokenSource;
+
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void OnStartButtonClicked(object sender, RoutedEventArgs e)
+        {
+            this.startButton.IsEnabled = false;
+            this.cancelButton.IsEnabled = true;
+
+            try
+            {
+                this.cancellationTokenSource = new CancellationTokenSource();
+
+                var token = this.cancellationTokenSource.Token;
+
+                await Task.Delay(TimeSpan.FromSeconds(5), token);
+
+                MessageBox.Show("Delay completed successfully.");
+            }
+            catch (OperationCanceledException)
+            {
+                MessageBox.Show("Delay was canceled.");
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Delay completed with error.");
+            }
+            finally
+            {
+                this.startButton.IsEnabled = true;
+                this.cancelButton.IsEnabled = false;
+            }
+        }
+
+        private void OnCancelButtonClicked(object sender, RoutedEventArgs e)
+        {
+            this.cancellationTokenSource?.Cancel();
+        }
+    }
+}
