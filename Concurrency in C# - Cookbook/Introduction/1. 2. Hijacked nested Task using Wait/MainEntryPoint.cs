06a7577eeef53cedbecd7e17b9dea0b1e32ad18c
flextry <flextry@gmail.com>
2017-06-03 18:38:21 +0300

    Book: Concurrency in C# - Cookbook
    1) Added demo for - Hijacked nested Task using Wait

---
+++ Concurrency in C# - Cookbook/Introduction/1. 2. Hijacked nested Task using Wait/MainEntryPoint.cs
@@ -0,0 +1,34 @@
+﻿namespace HijackedNestedTaskUsingWait
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// This is article from StackOverFlow - "Is Task.Factory.StartNew() guaranteed to use 
+    /// another thread than the calling thread?"
+    /// See <see cref="http://stackoverflow.com/questions/12245935/is-task-factory-startnew-guaranteed-to-use-another-thread-than-the-calling-thr" />
+    /// </summary>
+    public class MainEntryPoint
+    {
+        internal static void Main()
+        {
+            for (var i = 0; i < 10; i++)
+            {
+                Task.Factory.StartNew(Launch).Wait();
+            }
+        }
+
+        internal static void Launch()
+        {
+            Console.WriteLine("Launch thread: {0}", Thread.CurrentThread.ManagedThreadId);
+
+            Task.Factory.StartNew(Nested).Wait();
+        }
+
+        internal static void Nested()
+        {
+            Console.WriteLine("Nested thread: {0}\n", Thread.CurrentThread.ManagedThreadId);
+        }
+    }
+}
--- Concurrency in C# - Cookbook/Introduction/1. 2. Intro to Async Programming/MainEntryPoint.cs
+++ Concurrency in C# - Cookbook/Introduction/1. 2. Intro to Async Programming/MainEntryPoint.cs
@@ -2,18 +2,31 @@
 {
     using System;
     using System.Threading.Tasks;
+    using Nito.AsyncEx;
 
     public class MainEntryPoint
     {
         internal static void Main()
         {
-            MainEntryPoint.DoSomeWorkAsync();
-            MainEntryPoint.DoSomeWorkAsync();
-
-            Console.ReadLine();
+            try
+            {
+                AsyncContext.Run(async () => await MainAsync());
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex.Message);
+            }
         }
 
-        internal static async Task DoSomeWorkAsync()
+        private static async Task MainAsync()
+        {
+            var task1 = DoSomeWorkAsync();
+            var task2 = DoSomeWorkAsync();
+
+            await Task.WhenAll(task1, task2);
+        }
+
+        private static async Task DoSomeWorkAsync()
         {
             Console.WriteLine(1);
 
